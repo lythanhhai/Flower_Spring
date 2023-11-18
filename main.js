d46c8cfaa985387ec9de6148e59cf87cba923ee1
lythanhhai <lythanhhait@gmail.com>
2023-11-18 22:31:17 +0700

    update code

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-  
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
+
+const hearts = document.getElementsByClassName('material-symbols-outlined');
+const text = document.getElementsByClassName("Text")[0];
+for(let i = 0; i < hearts.length; i++)
+{
+  var count = 0;
+  hearts[i].addEventListener('click', () => {
+    const newtext = document.createTextNode("Chúc cậu có một ngày sinh nhật thật ý nghĩa, mãi hạnh phúc và thành công nhá !!! ");
+    const oldText = document.createTextNode("Chạm vào trái tim bất kỳ nhaa!!");
+    if(count == 0)
+    {
+      text.appendChild(newtext);
+      count++;
+      setTimeout(() => {
+        text.removeChild(newtext);
+        count = 0;
+        // alert("a")
+      }, 5000);
+    }
+  })
+}
